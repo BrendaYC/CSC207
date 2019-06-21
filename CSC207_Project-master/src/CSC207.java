a36537a6eb0b677f097f8710df4e637714ae6cc4
cheny402 <oliveryi.chen@mail.utoronto.ca>
2019-06-21 14:57:39 -0400

    JavaFX

--- CSC207_Project-master/src/CSC207.java
+++
@@ -1,8 +0,0 @@
-public class CSC207 {
-    public static void main(String[] args) {
-        System.out.println("CSC207");
-        System.out.println("CSC207");
-        System.out.println("CSC207");
-
-    }
-}
---
+++ src/JavaFX_GUI/Main.java
@@ -0,0 +1,11 @@
+package JavaFX_GUI;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hey!");
+
+        Application.launch(MyFirstApp.class, args);
+    }
+}
